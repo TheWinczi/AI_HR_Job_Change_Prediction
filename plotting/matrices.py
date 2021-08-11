99a786c06e006f6a13ce55164f09a5831519d39d
TheWinczi <mwinczewski@tutanota.com>
2021-08-11 20:50:23 +0200

    plotting confusion matrices of deep neural network has been made possible.

--- plotting/matrices.py
+++ plotting/matrices.py
@@ -1,11 +1,13 @@
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 
 def plot_confusion_matrix(clf,
                           x: np.ndarray,
-                          y_true: np.ndarray):
+                          y_true: np.ndarray,
+                          title: str = None):
     """
     Plot confusion matrix on one figure.
 
@@ -20,8 +22,15 @@ def plot_confusion_matrix(clf,
 
     y_true : ndarray
         Array of true labels belongs to input x data.
+
+    title : str {default: None}
+        Title of plot. Program will add "Confusion Matrix of" at the beginning of given title.
     """
     y_pred = clf.predict(x)
+
+    if isinstance(clf, tf.keras.Sequential):
+        y_pred = list(map(lambda item: 0 if item[0] <= 0.5 else 1, y_pred))
+
     confmat = confusion_matrix(y_true, y_pred)
 
     plt.matshow(confmat, alpha=0.3)
@@ -33,4 +42,5 @@ def plot_confusion_matrix(clf,
 
     plt.xlabel("Predicted Label")
     plt.ylabel("Real Label")
+    plt.title(f"Confusion Matrix of {title}")
     plt.show()
